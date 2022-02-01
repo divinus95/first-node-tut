c744ab336be4505aba978c8f7a497eef7fc4e4b0
Kwarteng Isaac Divinus <kwarteng.divinus@gmail.com>
2022-02-01 08:19:29 +0000

    chore(api): add patch endpoint

--- api/routes/products.js
+++ api/routes/products.js
@@ -19,7 +19,6 @@ router.get('/', (req, res, next) => {
                 console.log(err);
                 res.status(500).json({error: err})
             });
-    
 });
 
 router.post('/', (req, res, next) => {
@@ -46,11 +45,43 @@ router.get('/:productId', (req, res, next) => {
                     res.status(404).json({message: "No data found"})
                 }
             })
-            
             .catch(err => {
                 console.log(err);
                 res.status(500).json({error: err})
             });
+});
+
+router.patch('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    const updateOpts = {};
+    for(const opts of req.body){
+        updateOpts[opts.propName]= opts.value;
+    }
+    // Product.updateOne({_id: id}, {$set: {name: req.body.name, price: req.body.price}})
+    Product.update({_id: id}, {$set: updateOpts})
+            .exec()
+            .then(result => {
+                console.log(result);
+                res.status(200).json(result)
+            })
+            .catch(err => {
+                res.status(500).json({error: err})
+          
+            })
+})
+router.delete('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    Product.remove({_id: id})
+            .exec()
+            .then(result => {
+                res.status(200).json({result})
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({
+                    error: err
+                })
+            })
 });
 router.delete('/:productId', (req, res, next) => {
     const Id = req.params.productId;
